571f2409816db813f45c83b852d3d63f4731d822
Esteban Ordano <eordano@gmail.com>
2017-10-01 15:44:49 -0300

    Update LAND.sol

--- contracts/LAND.sol
+++ contracts/LAND.sol
@@ -3,4 +3,28 @@ pragma solidity ^0.4.15;
 contract Land is BasicNFT {
   name = 'Decentraland World';
   symbol = 'LAND';
+  
+  address claimContract;
+  mapping (uint => uint) latestPing;
+  
+  Land(address _claimContract) {
+    claimContract = _claimContract;
+  }
+  
+  assignNewParcel(address beneficiary, uint tokenId) {
+    require(msg.sender == claimContract);
+    require(!tokenOwner[tokenId]);
+    latestPing[tokenId] = now;
+    _addTokenTo(beneficiary, tokenId);
+  }
+  
+  ping(uint tokenId) {
+    require(msg.sender == tokenOwner[tokenId]);
+    latestPing[tokenId] = now;
+  }
+  
+  claimForgottenParcel(address beneficiary, uint tokenId) {
+    require(now - latestPing[tokenId] > 1 year);
+    _transfer(tokenOwner[tokenId], beneficiary, tokenId);
+  }
 }
