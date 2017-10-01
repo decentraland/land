1dbf41e3d38bd46820306cce1400b7e7dabdf448
Esteban Ordano <eordano@gmail.com>
2017-10-01 15:48:54 -0300

    Add events to LAND

--- contracts/LAND.sol
+++ contracts/LAND.sol
@@ -7,24 +7,31 @@ contract Land is BasicNFT {
   address claimContract;
   mapping (uint => uint) latestPing;
   
+  event TokenPing(uint tokenId);
+  
   Land(address _claimContract) {
     claimContract = _claimContract;
   }
   
-  assignNewParcel(address beneficiary, uint tokenId) {
+  assignNewParcel(address beneficiary, uint tokenId, bytes metadata) {
     require(msg.sender == claimContract);
     require(!tokenOwner[tokenId]);
     latestPing[tokenId] = now;
     _addTokenTo(beneficiary, tokenId);
+    TokenCreated(tokenId, beneficiary, metadata);
   }
   
   ping(uint tokenId) {
     require(msg.sender == tokenOwner[tokenId]);
     latestPing[tokenId] = now;
+    TokenPing(tokenId);
   }
   
   claimForgottenParcel(address beneficiary, uint tokenId) {
+    require(tokenOwner[tokenId] != 0);
+    require(latestPing[tokenId] < now);
     require(now - latestPing[tokenId] > 1 year);
     _transfer(tokenOwner[tokenId], beneficiary, tokenId);
+    TokenTransferred(tokenId, from, to);
   }
 }
