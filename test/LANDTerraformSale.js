c483a4eba6d3f25398ec1fb0cadba3c843c2b3e7
Ariel <abarmat@gmail.com>
2017-11-01 20:54:42 -0300

    Update Terraform test with bulk call

--- test/LANDTerraformSale.js
+++ test/LANDTerraformSale.js
@@ -6,48 +6,66 @@ const should = require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should()
 
+const utils = require('./utils')
+
 const Mana = artifacts.require('./FAKEMana')
 const Land = artifacts.require('./LANDToken')
 const LANDTerraformSale = artifacts.require('./LANDTerraformSale')
 
-contract('LANDTerraformSale', function ([owner, buyer1, buyer2]) {
-  let mana, sell, world
+contract('LANDTerraformSale', function ([owner, terraformReserve, buyer1, buyer2]) {
+  const totalMANALocked = 1e22
+  const returnRegistry = 0x0
+
+  let mana, sale, world
 
   before(async () => {
     mana = await Mana.new()
-    sell = await LANDTerraformSale.new(mana.address)
-    world = await Land.at(await sell.land())
-    await mana.setBalance(buyer1, 1e22)
-  })
+    sale = await LANDTerraformSale.new(mana.address, terraformReserve, returnRegistry)
+    world = await Land.at(await sale.land())
 
-  it('should have a genesis LAND', async function () {
-    const numberOfLand = await world.totalSupply()
-    numberOfLand.should.be.bignumber.equal(new BigNumber(1))
+    await mana.setBalance(terraformReserve, totalMANALocked)
+    await mana.approve(sale.address, totalMANALocked, {from: terraformReserve})
   })
 
   describe('buyer want to buy LAND', function () {
-    const metadata = 'Hello Decentraland!'
     const landCost = 1e21
 
-    it('should allow assign LAND to buyer', async function () {
-      await mana.approve(sell.address, landCost, { from: buyer1 })
-      await sell.buy(0, 1, metadata, landCost, { from: buyer1 })
+    it('should assign LAND to buyer', async function () {
+      await sale.buy(buyer1, 0, 0, landCost, { from: owner })
+
+      const numberOfLand = await world.totalSupply()
+      numberOfLand.should.be.bignumber.equal(new BigNumber(1))
+    })
+
+    it('should assign non-adjacent LAND to buyer', async function () {
+      await sale.buy(buyer1, 0, 10, landCost, { from: owner })
 
       const numberOfLand = await world.totalSupply()
       numberOfLand.should.be.bignumber.equal(new BigNumber(2))
     })
 
-    it('should allow assign non-adjacent LAND to buyer', async function () {
-      await mana.approve(sell.address, landCost, { from: buyer1 })
-      await sell.buy(0, 10, metadata, landCost, { from: buyer1 })
+    it('should assign LAND to buyer in bulk', async function () {
+      const x = [1, 1, 1, 1]
+      const y = [0, 1, 2, 3]
+      const cost = Array(x.length).fill(landCost)
+      const totalCost = utils.sum(cost)
 
+      const oldBalance = await mana.allowance(terraformReserve, sale.address)
+
+      await sale.buyMany(buyer2, x, y, totalCost)
+
+      // Check LAND assigned
       const numberOfLand = await world.totalSupply()
-      numberOfLand.should.be.bignumber.equal(new BigNumber(3))
+      numberOfLand.should.be.bignumber.equal(new BigNumber(2 + x.length))
+
+      // Check MANA burnt
+      const newBalance = await mana.allowance(terraformReserve, sale.address)
+      newBalance.should.be.bignumber.equal(oldBalance - totalCost)
     })
 
-    it('should recover right metadata', async function () {
-      const storedMetadata = await world.landMetadata(0, 1)
-      storedMetadata.should.be.equal(metadata)
+    it('should recover empty metadata', async function () {
+      const storedMetadata = await world.landMetadata(0, 0)
+      storedMetadata.should.be.equal('')
     })
   })
 })
