36c3163992018172f3e18e60abd1e15173dce29c
Ariel <abarmat@gmail.com>
2017-11-02 00:50:29 -0300

    Fix LANDToken tests

--- test/LANDToken.js
+++ test/LANDToken.js
@@ -1,30 +1,36 @@
-const Land = artifacts.require('./LANDToken')
-const BigNumber = web3.BigNumber
+'use strict'
 
-contract('LANDToken', function (accounts) {
+const BigNumber = web3.BigNumber
+const should = require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should()
+
+const Land = artifacts.require('./LANDToken')
+
+contract('LANDToken', function ([owner, user1, user2]) {
   let world
-  let land
-  let user = accounts[1]
-  let user2 = accounts[2]
-  let query
 
   beforeEach(async () => {
     world = await Land.new()
-    land = 1
-    await world.assignNewParcel(user, new BigNumber(1), 'Hello world')
+
+    const tokenId = await world.buildTokenId(0, 0)
+    await world.assignNewParcel(user1, tokenId, 'Genesis')
   })
 
-  it('reports that the user has the correct amount of land', async function () {
-    const numberOfLand = (await world.totalSupply.call()).toString()
-    assert(numberOfLand === 1)
+  it('should report that the user has the correct amount of LAND', async function () {
+    const numberOfLand = await world.totalSupply()
+    numberOfLand.should.be.bignumber.equal(1)
   })
 
-  it('handles correctly the transfer of a token', async function () {
-    await world.transfer(user2, land, { from: user })
-    let user1land = (await world.balanceOf(user)).toString()
-    let user2land = (await world.balanceOf(user2)).toString()
+  it('should handle the transfer of LAND', async function () {
+    const tokenId = await world.buildTokenId(0, 0)
+    await world.transfer(user2, tokenId, { from: user1 })
 
-    assert(user1land === 0)
-    assert(user2land === 1)
+    const balanceUser1 = await world.balanceOf(user1)
+    balanceUser1.should.be.bignumber.equal(0)
+
+    const balanceUser2 = await world.balanceOf(user2)
+    balanceUser2.should.be.bignumber.equal(1)
   })
 })
