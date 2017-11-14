1bce9ffc06e048c888c7e4fa497ae31ac65fee80
Ariel <abarmat@gmail.com>
2017-11-14 18:06:27 -0300

    Update truffle config for testnet

--- truffle.js
+++ truffle.js
@@ -1,6 +1,9 @@
 require('babel-register');
 require('babel-polyfill');
 
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const mnemonic = ''
+
 module.exports = {
   networks: {
     development: {
@@ -8,6 +11,11 @@ module.exports = {
       port: 8545,
       gas: 10000000,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/'),
+      network_id: 3, // official id of the ropsten network
+      gas: 3000000 
     }
   }
 };
