39cea686bf2bf9b1c70b03354536c5e4cc6f88d9
Esteban Ordano <eordano@gmail.com>
2017-11-16 11:46:25 -0300

    Update LANDToken.js

--- test/LANDToken.js
+++ test/LANDToken.js
@@ -37,9 +37,10 @@ contract('LANDToken', function ([owner, user1, user2]) {
   it('should allow updating multiple LAND updates', async function () {
     const land1 = await world.buildTokenId(1, 3)
     const land2 = await world.buildTokenId(-1, 2)
-    const land2 = await world.buildTokenId(5, 4)
+    const land3 = await world.buildTokenId(5, 4)
     await world.assignNewParcel(user1, land1, 'Empty')
     await world.assignNewParcel(user1, land2, 'Empty')
+    await world.assignNewParcel(user1, land3, 'Empty')
 
     await world.updateManyLandMetadata([1, -1, 5], [3, 2, 4], 'Changed', { from: user1 })
 
@@ -47,7 +48,7 @@ contract('LANDToken', function ([owner, user1, user2]) {
     resultLand1.should.equal('Changed')
     const resultLand2 = await world.landMetadata(-1, 2)
     resultLand2.should.equal('Changed')
-    const resultLand2 = await world.landMetadata(5, 4)
-    resultLand2.should.equal('Changed')
+    const resultLand3 = await world.landMetadata(5, 4)
+    resultLand3.should.equal('Changed')
   })
 })
