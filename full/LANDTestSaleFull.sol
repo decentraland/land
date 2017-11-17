75f3b0c1cbd59c692b3e1eb0e1ab5480f0bda279
Esteban Ordano <esteban@decentraland.org>
2017-11-17 14:38:27 -0300

    Add full contracts

--- full/LANDTestSaleFull.sol
+++ full/LANDTestSaleFull.sol
@@ -37,6 +37,23 @@ contract Ownable {
 
 }
 
+contract LANDTestSale {
+
+  LANDToken public land;
+
+  function LANDTestSale(address _land) {
+    land = LANDToken(_land);
+  }
+
+  function buy(uint256 _x, uint256 _y, string _data) public {
+    land.assignNewParcel(msg.sender, land.buildTokenId(_x, _y), _data);
+  }
+
+  function claimForgottenParcel(address beneficiary, uint tokenId) public {
+    land.claimForgottenParcel(beneficiary, tokenId);
+  }
+}
+
 contract NFT {
   function totalSupply() constant returns (uint);
   function balanceOf(address) constant returns (uint);
