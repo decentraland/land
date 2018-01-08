676700f03c9583c2f9ea2dfeb1f44cb1a5b147f6
Ariel <abarmat@gmail.com>
2018-01-08 00:20:30 -0300

    Terraform contract that performs buys with no burning

--- test/LANDTerraformSale.js
+++ test/LANDTerraformSale.js
@@ -8,34 +8,26 @@ const should = require('chai')
 
 const { EVMRevert, sum } = require('./utils')
 
-const Mana = artifacts.require('./FAKEMana')
-const Land = artifacts.require('./LANDToken')
+const MANAToken = artifacts.require('./FAKEMana')
+const LANDRegistry = artifacts.require('./LANDToken')
 const LANDContinuousSale = artifacts.require('./LANDContinuousSale')
 const LANDTerraformSale = artifacts.require('./LANDTerraformSale')
-const ReturnVestingRegistry = artifacts.require('./ReturnVestingRegistry')
 
 contract('LANDTerraformSale', function ([owner, terraformReserve, buyer1, buyer2, vested2]) {
-  const landCost = 1000 * 1e18
-  const totalMANALocked = 10000 * 1e18
+  const metadata = ''
 
-  let mana, sale, world, returnVesting
+  let mana, sale, world
 
   beforeEach(async () => {
-    mana = await Mana.new()
-    returnVesting = await ReturnVestingRegistry.new()
-    await returnVesting.record(buyer2, vested2)
-
-    sale = await LANDTerraformSale.new(mana.address, terraformReserve, returnVesting.address)
-    world = await Land.at(await sale.land())
-
-    await mana.setBalance(terraformReserve, totalMANALocked)
-    await mana.approve(sale.address, totalMANALocked, {from: terraformReserve})
+    mana = await MANAToken.new()
+    sale = await LANDTerraformSale.new()
+    world = await LANDRegistry.at(await sale.land())
   })
 
   describe('buyer want to buy LAND', function () {
     it('should assign non-adjacent LAND to buyer', async function () {
-      await sale.buy(buyer1, 0, 0, landCost, { from: owner })
-      await sale.buy(buyer1, 0, 10, landCost, { from: owner })
+      await sale.buy(buyer1, 0, 0, { from: owner })
+      await sale.buy(buyer1, 0, 5, { from: owner })
 
       // Check LAND created
       const numberOfLand = await world.totalSupply()
@@ -47,19 +39,15 @@ contract('LANDTerraformSale', function ([owner, terraformReserve, buyer1, buyer2
       newOwner = await world.ownerOfLand(0, 0)
       newOwner.should.be.equal(buyer1)
 
-      newOwner = await world.ownerOfLand(0, 10)
+      newOwner = await world.ownerOfLand(0, 5)
       newOwner.should.be.equal(buyer1)
     })
 
     it('should assign LAND to buyer in bulk', async function () {
       const x = [1, 1, 1, 1]
       const y = [0, 1, 2, 3]
-      const cost = Array(x.length).fill(landCost)
-      const totalCost = sum(cost)
 
-      const oldBalance = await mana.balanceOf(terraformReserve)
-      await sale.buyMany(buyer2, x, y, totalCost)
-      const newBalance = await mana.balanceOf(terraformReserve)
+      await sale.buyMany(buyer2, x, y)
 
       // Check LAND created
       const numberOfLand = await world.totalSupply()
@@ -71,9 +59,6 @@ contract('LANDTerraformSale', function ([owner, terraformReserve, buyer1, buyer2
         newOwner = await world.ownerOfLand(x[i], y[i])
         newOwner.should.be.equal(buyer2)
       }
-
-      // Check MANA burnt
-      newBalance.should.be.bignumber.equal(oldBalance - totalCost)
     })
 
     it('should recover empty metadata', async function () {
@@ -82,69 +67,20 @@ contract('LANDTerraformSale', function ([owner, terraformReserve, buyer1, buyer2
     })
   })
 
-  describe('return MANA back to buyers', function () {
-    it('should throw if not the owner returning funds', async function () {
-      await sale.transferBackMANA(buyer1, landCost, {from: buyer2}).should.be.rejectedWith(EVMRevert)
-    })
-
-    it('should throw if amount to return is not positive', async function () {
-      await sale.transferBackMANA(buyer1, 0).should.be.rejectedWith(EVMRevert)
-      await sale.transferBackMANA(buyer1, -1).should.be.rejectedWith(EVMRevert)
-    })
-
-    it('should throw if return address is invalid', async function () {
-      await sale.transferBackMANA(0x0, landCost).should.be.rejectedWith(EVMRevert)
-    })
-
-    it('should return funds to buyer', async function () {
-      const oldBalance = await mana.balanceOf(buyer1)
-      await sale.transferBackMANA(buyer1, landCost)
-      const newBalance = await mana.balanceOf(buyer1)
-      newBalance.should.be.bignumber.equal(oldBalance + landCost)
-    })
-
-    it('should return funds to vested buyer', async function () {
-      const oldBalance = await mana.balanceOf(vested2)
-      await sale.transferBackMANA(buyer2, landCost)
-      const newBalance = await mana.balanceOf(vested2)
-      newBalance.should.be.bignumber.equal(oldBalance + landCost)
-    })
-
-    it('should transfer funds in bulk and take into account vesting accounts', async function () {
-      // before state
-      const oldBalance1 = await mana.balanceOf(buyer1)
-      const oldBalance2 = await mana.balanceOf(vested2)
-
-      // transfer
-      const addresses = [buyer1, buyer2]
-      const amounts = [landCost, landCost]
-      await sale.transferBackMANAMany(addresses, amounts)
- 
-      // after state
-      const newBalance1 = await mana.balanceOf(buyer1)
-      const newBalance2 = await mana.balanceOf(vested2)
-      newBalance1.should.be.bignumber.equal(oldBalance1 + landCost)
-      newBalance2.should.be.bignumber.equal(oldBalance2 + landCost)
-    })
-
-    it('should throw if addresses and amounts have different length', async function () {
-      const addresses = [buyer1, buyer2]
-      const amounts = [landCost]
-      await sale.transferBackMANAMany(addresses, amounts).should.be.rejectedWith(EVMRevert) 
-    })
-  })
-
   describe('transfer ownership of LANDToken contract', function () {
-    it('should allow transfering ownership of LANDToken contract', async function () {
-      const metadata = ''
-    
-      await sale.buy(buyer1, 0, 0, landCost, { from: owner })
+    it('should allow transfering ownership of LANDToken contract', async function () {    
+      const landCost = 1000 * 1e18
 
-      await mana.setBalance(buyer1, landCost)
+      // terraform contract buy ok
+      await sale.buy(buyer1, 0, 0, { from: owner })
+
+      // continuous sale buy is not ok until transferred ownership
       const newSale = await LANDContinuousSale.new(mana.address, world.address)
+      await mana.setBalance(buyer1, landCost)
       await mana.approve(newSale.address, landCost, { from: buyer1 })
       await newSale.buy(0, 1, metadata, { from: buyer1 }).should.be.rejectedWith(EVMRevert)
 
+      // allow buy after transfering ownership
       await sale.transferLandOwnership(newSale.address)
       await newSale.buy(0, 1, metadata, { from: buyer1 }).should.not.be.rejectedWith(EVMRevert)
     })
