5288f6a0f1bbee10cb7f4a402c0d6ce434dd4b6c
Esteban Ordano <esteban@decentraland.org>
2018-03-18 17:44:51 -0300

    test: add minor integration test

---
+++ test/Integration.js
@@ -0,0 +1,78 @@
+import assertRevert from './helpers/assertRevert'
+import { increaseTimeTo, duration, latestTime } from './helpers/increaseTime'
+
+const BigNumber = web3.BigNumber
+
+const LANDRegistry = artifacts.require('LANDRegistryTest')
+const LANDProxy = artifacts.require('LANDProxy')
+
+const NONE = '0x0000000000000000000000000000000000000000'
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should()
+
+contract('LANDRegistry', accounts => {
+  const [creator, user, anotherUser, operator, hacker] = accounts
+  let registry = null,
+    proxy = null
+  let land = null
+  const _name = 'Decentraland LAND'
+  const _symbol = 'LAND'
+  const sentByUser = { from: user }
+  const sentByCreator = { from: creator }
+  const creationParams = {
+    gas: 6e6,
+    gasPrice: 21e9,
+    from: creator
+  }
+
+  beforeEach(async function() {
+    proxy = await LANDProxy.new(creationParams)
+    registry = await LANDRegistry.new(creationParams)
+
+    await proxy.upgrade(registry.address, creator, sentByCreator)
+    land = await LANDRegistry.at(proxy.address)
+    await land.initialize(creator, sentByCreator)
+    await land.authorizeDeploy(creator, sentByCreator)
+    await land.ping(sentByUser)
+  })
+
+  async function assign({ to, asset, initialValue }) {
+    await land.assignNewParcel(0, asset, to, sentByCreator)
+    await land.updateLandData(0, asset, initialValue, { from: to })
+  }
+
+  async function transfer({ from, to, asset }) {
+    await land.transferLand(0, asset, to, { from })
+  }
+
+  async function update({ from, asset, value }) {
+    await land.updateLandData(0, asset, value, { from })
+  }
+
+  const assetOne = 1
+  const initialValue = 'initial'
+  const newValue = 'new'
+
+  describe('Combinations of calls', () => {
+    it('before transfer, update is possible, after, it is not', async () => {
+      await assign({to: user, asset: assetOne, initialValue: initialValue})
+      await update({from: user, asset: assetOne, value: newValue})
+      await transfer({from: user, to: anotherUser, asset: assetOne})
+      await assertRevert(update({from: user, asset: assetOne, value: newValue}))
+    })
+    it('before owning, update is impossible, after, it is not', async () => {
+      await assign({to: user, asset: assetOne, initialValue: initialValue})
+      await assertRevert(update({from: anotherUser, asset: assetOne, value: newValue}))
+      await transfer({from: user, to: anotherUser, asset: assetOne})
+      await update({from: anotherUser, asset: assetOne, value: newValue})
+    })
+    /**
+     * - Setup old contract and test upgrades
+     * - Check for updates and
+     */
+  })
+
+})
