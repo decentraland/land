161f3b7d30ee279f064652f2719eb5bb173254cc
Ariel <abarmat@decentraland.org>
2018-08-22 12:41:16 -0300

    chore: update truffle config

---
+++ truffle-config.js
@@ -0,0 +1,37 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const mnemonic = '' // 12 word mnemonic
+
+module.exports = {
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  networks: {
+    livenet: {
+      host: 'localhost',
+      port: 8545,
+      gas: 70000000,
+      network_id: '*'
+    },
+    development: {
+      host: 'localhost',
+      port: 18545,
+      gas: 100000000,
+      network_id: '*'
+    },
+    ropsten: {
+      host: 'localhost',
+      port: 18545,
+      network_id: 3,
+      gas: 30000000
+    },
+    infura_ropsten: {
+      provider: () =>
+        new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/'),
+      network_id: 3,
+      gas: 30000000
+    }
+  }
+}
