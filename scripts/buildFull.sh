cee38e435e6fd93af41521b92549a81a9730e53c
Ignacio Mazzara <nachomazzara@gmail.com>
2018-08-29 16:08:16 -0300

    feat: upgrade estate registry (#99)

--- scripts/buildFull.sh
+++ scripts/buildFull.sh
@@ -2,8 +2,12 @@
 
 REGISTRY=LANDRegistry.sol
 PROXY=LANDProxy.sol
+ESTATE_REGISTRY=EstateRegistry.sol
+
 
 OUTPUT=full
 
 truffle-flattener contracts/land/$REGISTRY > $OUTPUT/$REGISTRY
 truffle-flattener contracts/upgradable/$PROXY > $OUTPUT/$PROXY
+truffle-flattener contracts/estate/$ESTATE_REGISTRY > $OUTPUT/$ESTATE_REGISTRY
+
