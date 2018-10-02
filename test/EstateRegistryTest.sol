b6c17cc79bdfa4049e5954a8bd886d3b25da7efa
Esteban Ordano <esteban@decentraland.org>
2018-10-02 08:49:51 -0300

    fix: add fingerprint salt to avoid collision

--- test/EstateRegistryTest.sol
+++ test/EstateRegistryTest.sol
@@ -25,6 +25,16 @@ contract EstateRegistryTest is EstateRegistry {
     return keccak256(abi.encodePacked(x)) ^ keccak256(abi.encodePacked(y));
   }
 
+  function calculateEstateHash(uint256 estateId, uint256[] tokens) public pure returns (bytes32) {
+    bytes32 result = keccak256(abi.encodePacked('estateId', estateId));
+
+    uint256 length = tokens.length;
+    for (uint i = 0; i < length; i++) {
+      result ^= keccak256(abi.encodePacked(tokens[i]));
+    }
+    return result;
+  }
+
   function compoundXor(bytes32 x, uint256 y) public pure returns (bytes32) {
     return x ^ keccak256(abi.encodePacked(y));
   }
