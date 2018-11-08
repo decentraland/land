3db8314c666a361639c04c2497b0172f5aebfd58
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-11-08 20:40:06 +0100

    feat: assign parcels owner

---
+++ deploy/assignParcels.js
@@ -0,0 +1,171 @@
+const {
+  log,
+  setLogLevel,
+  expandPath,
+  parseArgs,
+  getConfiguration,
+  readJSON,
+  isEmptyAddress
+} = require('./utils')
+
+const LANDRegistryArtifact = artifacts.require('LANDRegistry')
+const LANDRegistryDecorator = require('./LANDRegistryDecorator')
+
+const BATCH_SIZE = 50
+const REQUIRED_ARGS = ['parcels', 'account', 'password', 'owner']
+
+function checkRequiredArgs(args) {
+  const hasRequiredArgs = REQUIRED_ARGS.every(argName => args[argName] != null)
+
+  if (!hasRequiredArgs) {
+    const argNames = Object.keys(args)
+    throw new Error(
+      `Missing required arguments. Supplied ${argNames}, required ${REQUIRED_ARGS}`
+    )
+  }
+}
+
+function checkWeb3Account(account) {
+  if (web3 === 'undefined') {
+    throw new Error('web3 object is not defined')
+  }
+  if (!web3.eth.accounts || web3.eth.accounts.length === 0) {
+    throw new Error('Empty web3 accounts')
+  }
+  if (!web3.eth.accounts.find(ethAccount => ethAccount === account)) {
+    throw new Error(
+      `Couldn't find account ${account} in:\n${web3.eth.accounts.join('\n')}`
+    )
+  }
+}
+
+async function assignParcels(
+  parcels,
+  landRegistry,
+  newOwner,
+  batchSize = BATCH_SIZE
+) {
+  log.debug(`Setting the owner of ${parcels.length} parcels as ${newOwner}`)
+  const parcelsToAssign = []
+  const pendingTransactions = {}
+
+  for (const parcel of parcels) {
+    if (typeof parcel.x === 'undefined' || typeof parcel.y === 'undefined') {
+      throw new Error(
+        `Malformed parcel found on parcel json file: ${JSON.stringify(parcel)}`
+      )
+    }
+
+    log.debug(`Getting on chain owner for parcel ${parcel.x},${parcel.y}`)
+    const owner = await landRegistry.getCurrentOwner(parcel)
+
+    if (isEmptyAddress(owner)) {
+      log.debug(`Empty owner for ${parcel.x},${parcel.y}, will be assigned`)
+    } else {
+      log.warn(`${parcel.x},${parcel.y} already has ${owner} as owner!`)
+      continue
+    }
+
+    parcelsToAssign.push(parcel)
+
+    if (parcelsToAssign.length >= batchSize) {
+      log.warn(`Assigning ${newOwner} as owner for ${parcelsToAssign.length}`)
+      const hash = await landRegistry.assignMultipleParcels(
+        parcelsToAssign,
+        newOwner
+      )
+      pendingTransactions[hash] = parcelsToAssign
+      parcelsToAssign = []
+    }
+  }
+
+  if (parcelsToAssign.length > 0) {
+    const hash = await landRegistry.assignMultipleParcels(
+      parcelsToAssign,
+      newOwner
+    )
+    pendingTransactions[hash] = parcelsToAssign
+  }
+
+  log.info('Wating for all transactions to end')
+  const failedTransactions = await getFailedTransactions(
+    pendingTransactions,
+    web3
+  )
+  const failedTransactionsCount = Object.keys(failedTransactions).length
+
+  if (failedTransactionsCount > 0) {
+    log.info(
+      `Found ${failedTransactionsCount} failed transactions, retrying those`
+    )
+    const failedParcels = Object.values(failedTransactions).reduce(
+      (allParcels, parcels) => allParcels.concat(parcels),
+      []
+    )
+    return await assignParcels(parcels, landRegistry, newOwner, batchSize)
+  }
+}
+
+async function run(args) {
+  checkRequiredArgs(args)
+  checkWeb3Account(args.account)
+
+  setLogLevel(args.logLevel)
+  log.debug('Using args', JSON.stringify(args, null, 2))
+
+  const configuration = getConfiguration()
+  const parcelsToDeploy = readJSON(expandPath(args.parcels))
+
+  const { account, password, owner, batchSize } = args
+  const { txConfig, contractAddresses } = configuration
+
+  log.debug(`Unlocking account ${account}`)
+  await this.web3.personal.unlockAccount(account, password, 10000)
+
+  const landRegistryContract = await LANDRegistryArtifact.at(
+    contractAddresses.LANDRegistry
+  )
+  const landRegistry = new LANDRegistryDecorator(
+    landRegistryContract,
+    account,
+    txConfig
+  )
+
+  await assignParcels(parcelsToDeploy, landRegistry, owner, batchSize)
+}
+
+async function main(argv) {
+  try {
+    if (argv.length < REQUIRED_ARGS.length || argv[0] === 'help') {
+      console.log(`Deploy (set an owner for) a list of unowned parcels. To run, use:
+
+truffle exec assignParcels.js --parcels genesis.json --account 0x --password 123 --batch 50 --owner 0x --logLevel debug --network (...)
+
+--parcels genesis.json - List of parcels to deploy. Required
+--account 0xdeadbeef     - Which account to use to deploy. Required
+--password S0m3P4ss      - Password for the account. Required
+--batchSize 50           - Parcels per transaction. Default ${BATCH_SIZE}
+--owner 0xdeadbeef       - The new owner to be used. Required
+--logLevel debug         - Log level to use. Possible values: info, debug. Default: info
+
+`)
+    } else {
+      await run(parseArgs(argv))
+    }
+  } catch (error) {
+    log.error(error)
+    throw new Error(error)
+  }
+}
+
+if (require.main === module) {
+  main(process.argv.slice(2))
+}
+
+module.exports = async function(callback) {
+  try {
+    await main(process.argv.slice(4))
+  } finally {
+    callback()
+  }
+}
--- deploy/utils.js
+++ deploy/utils.js
@@ -5,6 +5,9 @@ const log = {
   info(...args) {
     console.log('[INFO]', ...args)
   },
+  warn(...args) {
+    console.log('[WARN]', ...args)
+  },
   debug() {},
   error(...args) {
     console.error('[ERROR]', ...args)
@@ -17,6 +20,10 @@ function setLogLevel(logLevel = 'info') {
       console.log('[DEBUG]', ...args)
     }
   }
+}
+
+function expandPath(path) {
+  return ['.', '/'].includes(path[0]) ? path : `${__dirname}/${path}`
 }
 
 function parseArgs(args) {
@@ -64,11 +71,50 @@ function requestJSON(url) {
   })
 }
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+async function getFailedTransactions(allPendingTransactions, web3) {
+  const pendingTransactions = { ...allPendingTransactions }
+  const failedTransactions = {}
+
+  while (Object.keys(pendingTransactions).length > 0) {
+    for (const hash in pendingTransactions) {
+      const tx = await web3.eth.getTransaction(hash)
+      log.debug(`Getting status of tx ${hash}, got`, tx)
+
+      if (!tx.blockNumber) continue
+      await sleep(1000)
+
+      const receipt = await web3.eth.getTransactionReceipt(hash)
+      log.debug(`Getting receipt of tx ${hash}, got`, receipt)
+
+      if (receipt == null || receipt.status === '0x0') {
+        failedTransactions[hash] = pendingTransactions[hash]
+      }
+      delete pendingTransactions[hash]
+    }
+
+    await sleep(5000)
+  }
+
+  return failedTransactions
+}
+
+function isEmptyAddress(address) {
+  const NULL = '0x0000000000000000000000000000000000000000'
+  const NULL_PARITY = '0x'
+  return address === NULL || address === NULL_PARITY
+}
+
 module.exports = {
   log,
   setLogLevel,
+  expandPath,
   parseArgs,
   getConfiguration,
   readJSON,
-  requestJSON
+  requestJSON,
+  sleep,
+  getFailedTransactions,
+  isEmptyAddress
 }
