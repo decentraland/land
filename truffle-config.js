f6795c4e47564b08d39d04f2f2d0f60fa97b69ac
Ignacio Mazzara <nachomazzara@gmail.com>
2019-01-28 14:25:30 -0300

    chore: upgrade contracts (#130)

--- truffle-config.js
+++ truffle-config.js
@@ -34,6 +34,7 @@ module.exports = {
     },
     ropsten: {
       host: 'localhost',
+      from: '0x62ba62ff92917edf8ac0386fa10e3b27950bce8d',
       port: 8545,
       network_id: 3,
       gas: 30000000
