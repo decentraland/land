e4533a7a60bda7feabbe0e70b0466aafbd588ed9
Ignacio Mazzara <nacho@decentraland.org>
2019-02-23 20:07:04 -0300

    fix: updateOperator event emission

--- test/EstateRegistryTest.sol
+++ test/EstateRegistryTest.sol
@@ -28,4 +28,20 @@ contract EstateRegistryTest is EstateRegistry {
   function compoundXor(bytes32 x, uint256 y) public pure returns (bytes32) {
     return x ^ keccak256(abi.encodePacked(y));
   }
+
+  function safeTransferFromWithBytes(
+    address from, 
+    address to, 
+    uint256 assetId, 
+    bytes data
+  ) 
+    public 
+  {
+    safeTransferFrom(
+      from, 
+      to,
+      assetId, 
+      data
+    );
+  }
 }
